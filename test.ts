de8565a0404ea2491bdfda9ed37b447285e5b434
samnied <43746162+samnied@users.noreply.github.com>
2019-10-16 22:14:39 +0200

    Aktualisierungen.

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 // tests go here; this will not be compiled when this package is used as a library
+TCS34725.start(TCS34725_ATIME.ATIME_2_4_MS, TCS34725_AGAIN.AGAIN_4X)
+basic.forever(function () {
+    serial.writeValue("r", TCS34725.getSensorData(RGB.RED))
+    serial.writeValue("g", TCS34725.getSensorData(RGB.GREEN))
+    serial.writeValue("b", TCS34725.getSensorData(RGB.BLUE))
+    serial.writeValue("c", TCS34725.getSensorData(RGB.CLEAR))
+    basic.pause(250)
+})
